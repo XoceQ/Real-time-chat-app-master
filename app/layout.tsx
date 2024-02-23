f6b89a13351f753f90111dbf62e94cab4bcb4caa
Janus <josefernandorevelo@gmail.com>
2024-02-23 15:57:36 -0500

    Chat Messages Component commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import {cn} from "@/lib/utils";
 import {ModalProvider} from "@/components/providers/modal-provider";
 import {ThemeProvider} from "@/components/providers/theme-provider";
 import {SocketProvider} from "@/components/providers/socket-provider";
+import {QueryProvider} from "@/components/providers/query-provider";
 
 
 const font = Open_Sans({subsets: ['latin']})
@@ -21,6 +22,8 @@ export default function RootLayout({
                                    }: {
     children: React.ReactNode
 }) {
+
+
     return (
         <ClerkProvider>
             <html lang="en" suppressHydrationWarning>
@@ -35,11 +38,13 @@ export default function RootLayout({
                 storageKey="discord-theme">
                 <SocketProvider>
                     <ModalProvider/>
-                    {children}
+                    <QueryProvider>
+                        {children}
+                    </QueryProvider>
                 </SocketProvider>
             </ThemeProvider>
             </body>
             </html>
         </ClerkProvider>
-    )
+    );
 }
